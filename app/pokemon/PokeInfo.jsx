c1f5fe0e931dc1f2e91cac8f2a3e2b433e67763d
Giovanni Castañeda <giovacas98@hotmail.com>
2023-09-28 15:46:37 -0500

    Abilities fixed

--- app/pokemon/PokeInfo.jsx
+++ app/pokemon/PokeInfo.jsx
@@ -17,18 +17,18 @@ function PokeInfo({ data }) {
             width={75}
             height={75}
           />
-          {/*<div className="abilities m-auto flex justify-center items-center mt-2 gap-2">
+          <div className="abilities m-auto hidden md:flex justify-center items-center mt-2 gap-2">
             {data.abilities.map(poke => (
               <div className="group mr-2 lg:mx-0">
                 <h2 className='px-2 text-sm'>{poke.ability.name}</h2>
               </div>
             ))}
-          </div>*/}
+          </div>
           <div className="base-stat mt-2">
             {data.stats.map(poke => {
               return (
                 <div className='flex justify-center uppercase text-sm'>
-                  <h3 className='font-extralight'>
+                  <h3 className='font-extralight text-zinc-300'>
                     {poke.stat.name}:
                   </h3>
                   <span className='font-light'>{poke.base_stat}</span>
